a4b7c744014e3016a511828c7340ceefc7170a47
Md. Sohel Akter <sohelrakib929@gmail.com>
2024-05-15 12:08:14 +0600

    alter table add 2 column

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,30 @@
 const models = require('../models/index');
 
-
 exports.index = (req, res, next) => {
     models.User.findAll({
         // model: models.Address,
         // required: true
-        include: [models.Address]
+        // include: [models.Address],
+        include: [
+            {
+                model: models.Address,
+                required: false  // Include users even if they do not have associated addresses
+            }
+        ],
+        where: {
+            status: [1],
+        },
+        order: [
+            ['id', 'desc']
+            // [{ model: models.Address }, 'city', 'ASC']
+        ]
     })
     .then(users => {
-        console.log(JSON.stringify(users[0].Addresses[0].city, null, 2));
-        console.log(JSON.stringify(users, null, 2));
+        if (users.length > 0 && users[0].Addresses && users[0].Addresses.length > 0) {
+            console.log(JSON.stringify(users[0].Addresses[0].city, null, 2));
+        }
+        // console.log(JSON.stringify(users[0].Addresses[0].city, null, 2));
+        // console.log(JSON.stringify(users, null, 2));
         res.render('user/list', {
             users: users,
             pageTitle: 'All Users',
---
+++ migrations/20240515052941-add-dob-and-status-to-user.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addColumn('Users', 'dob', {
+      type: Sequelize.DATEONLY,
+      allowNull: true, // or false, depending on your requirements
+    });
+
+    await queryInterface.addColumn('Users', 'status', {
+      type: Sequelize.TINYINT,
+      allowNull: true, // or false, depending on your requirements
+    });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.removeColumn('Users', 'dob');
+    await queryInterface.removeColumn('Users', 'status');
+
+  }
+};
